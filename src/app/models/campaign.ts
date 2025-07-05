19abb2647bd666825651e1184cb4173751068ff8
Gilson Bezerra <gilson.bezerra@live.com>
2025-07-04 21:41:55 -0300

    ajuste

--- src/app/models/campaign.ts
+++ src/app/models/campaign.ts
@@ -1,13 +1,13 @@
 import { Client } from "./client";
 export interface Campaign {
   id: string;
+  name?: string;
   client: Client;
   createdAt: Date | string;
   createdById?: string;
   createdByName?: string;
   days: number;
   status?: string;
-  name?: string;
   price?: number;
   period?: string;
   mediaType?: string;
